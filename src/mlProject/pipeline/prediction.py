1b502fa49d353843c1ffec0abdeb9f681381773c
notevensmart <parth.college123@gmail.com>
2025-03-16 16:32:51 -0400

    updates

--- src/mlProject/pipeline/prediction.py
+++ src/mlProject/pipeline/prediction.py
@@ -2,15 +2,18 @@ import joblib
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from transformers import pipeline
 
-
-
+print("Model loaded")
 class PredictionPipeline:
     def __init__(self):
+        print("Model loaded")
         self.model = joblib.load(Path('artifacts/model_trainer/model.joblib'))
+        print("Model loaded")
 
     
     def predict(self, data):
         prediction = self.model.predict(data)
+        print("this is prediction:{prediction}")
 
         return prediction
